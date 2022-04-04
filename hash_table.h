77187c07fc291c334556c074846266f3665e4539
Peter McHale <peter.thomas.mchale@gmail.com>
2022-04-04 06:19:50 -0600

    add video for hast table

--- hash_table.h
+++ hash_table.h
@@ -3,6 +3,7 @@
 
 namespace hash_table {
 
+	  // https://www.youtube.com/watch?v=shs0KM3wKv8&list=PLOuZYwbmgZWXvkghUyMLdI90IwxbNCiWK&index=1
     void hash_table_example() {
         std::unordered_map<std::string, int> number_days;
         number_days["January"] = 31;
